ad414ef48156146b9de8f6d8c8f41eac99856bf4
pashogus <shansasi.28@gmail.com>
2020-09-19 16:34:21 +0530

    some changes to check

--- helloPashogus1.java
+++ helloPashogus1.java
@@ -2,6 +2,8 @@
 
 class helloPashogus {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        String check ="hello Pashogus";	
+        System.out.println(check1);		
     }
 }
