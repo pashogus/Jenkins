5f25b4204c1fbf8adcae1a7a9a97426fa64572cb
pashogus <shansasi.28@gmail.com>
2020-09-20 15:30:42 +0530

    testing of web hook with web hook with proxy public URL for local host

--- helloPashogus.java
+++ helloPashogus.java
@@ -4,6 +4,6 @@ class helloPashogus {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!!!!!!!"); 
 		//*/15 * * * *
-		
+		//http://192.168.1.10:9000/git/notifyCommit?url=file:///G:/Projects/Jenkins
     }
 }
